cda17db34c7947a7eddf590d5c97d7c40ac0712f
buiok <106178856+buiok@users.noreply.github.com>
2023-05-08 18:44:18 +0500

    готово

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
 
@@ -28,18 +26,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // String result = "0";
-  String expression = "";
   Color buttonBackColor = const Color.fromRGBO(23, 23, 23, 1.0);
 
-  String equation = ""; // строка для хранения уравнения
-  String result = ""; // строка для хранения результата
+  String equation = "";
+  String result = "0";
 
-  Map<String,String> config = {
-    '+' : '+',
-    '−' : '-',
-    '×' : '*',
-    '÷' : '/',
+  Map<String, String> config = {
+    '+': '+',
+    '−': '-',
+    '×': '*',
+    '÷': '/',
   };
 
   List<String> getKeysFromMap(Map map) {
@@ -48,10 +44,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
   calculateResult(equation) {
     try {
-      if (equation.contains('/0')) {
-        result = "Error";
-      }
-      double evalResult = Parser().parse(equation).evaluate(EvaluationType.REAL, ContextModel());
+      double evalResult = Parser()
+          .parse(equation)
+          .evaluate(EvaluationType.REAL, ContextModel());
       if (evalResult % 1 == 0) {
         result = evalResult.toInt().toString();
       } else {
@@ -61,18 +56,28 @@ class _MyHomePageState extends State<MyHomePage> {
     } catch (e) {
       result = "Error";
     }
+    if (result == 'Infinity' || result == 'NaN') {
+      result = "Error";
+    }
     return [equation, result];
   }
 
   buttonPressed(String buttonText) {
     setState(() {
-      // нельзя писать если есть 0 в equat
+      if (result == 'Error') {
+        result = "0";
+        equation = "";
+      }
       if (buttonText == "C") {
         equation = "";
         result = "0";
-      } else if (buttonText == "+" || buttonText == "−" || buttonText == "×" || buttonText == "÷") {
+      } else if (buttonText == "+" ||
+          buttonText == "−" ||
+          buttonText == "×" ||
+          buttonText == "÷") {
         buttonText = config[buttonText]!;
-        if (isOperator(equation.substring(equation.length - 1))) {
+        if (equation.isNotEmpty &&
+            isOperator(equation.substring(equation.length - 1))) {
           equation = equation.substring(0, equation.length - 1);
         }
         if (equation != "") {
@@ -80,12 +85,13 @@ class _MyHomePageState extends State<MyHomePage> {
           equation = calc[0];
           result = calc[1];
           equation += buttonText;
-        } else if (equation == "") {
+        } else {
           equation = "0$buttonText";
           result = "0";
         }
       } else if (buttonText == "+/-") {
-        if (result != "0" && !isOperator(equation.substring(equation.length - 1))) {
+        if (result != "0" &&
+            !isOperator(equation.substring(equation.length - 1))) {
           var num = result;
           double number = double.parse(result);
           number = -number;
@@ -94,8 +100,6 @@ class _MyHomePageState extends State<MyHomePage> {
           } else {
             result = number.toString();
           }
-          // result = number.toString(); //-1 -> 1
-          //можно добавлять result тока когда оператор нажали
           var lastIndexResult = equation.lastIndexOf(num);
           if (equation.contains("(-")) {
             lastIndexResult -= 1;
@@ -107,7 +111,9 @@ class _MyHomePageState extends State<MyHomePage> {
           }
         }
       } else if (buttonText == ".") {
-        if ((equation != "" && isOperator(equation.substring(equation.length - 1))) || equation == "") {
+        if ((equation != "" &&
+                isOperator(equation.substring(equation.length - 1))) ||
+            equation == "") {
           result = "0.";
           equation += "0.";
         } else if (!result.contains(".")) {
@@ -140,52 +146,45 @@ class _MyHomePageState extends State<MyHomePage> {
         }
       }
     });
-    // setState(() {
-    //   if (buttonText == "C") {
-    //     expression = "";
-    //     result = "0";
-    //   } else if (buttonText == "=") {
-    //     try {
-    //       Parser p = Parser();
-    //       Expression exp = p.parse(expression);
-    //       ContextModel cm = ContextModel();
-    //       result = '${exp.evaluate(EvaluationType.REAL, cm)}';
-    //     } catch (e) {
-    //       result = "Error";
-    //     }
-    //   } else {
-    //     expression = expression + buttonText;
-    //     if (!["+/-", ".", "÷", "×", "−", "+"].contains(buttonText)) {
-    //       result = expression;
-    //     }
-    //   }
-    // });
   }
 
   bool isOperator(String str) {
     return str == "+" || str == "-" || str == "*" || str == "/";
   }
 
-  Widget buildButton(
-      String buttonText, double buttonHeight, Color buttonColor, [double fontSize = 32]) {
+  Widget buildButton(String buttonText, Color buttonColor, {int flex = 1, double fontSize = 32}) {
     if (buttonText == '=') {
       buttonBackColor = const Color.fromRGBO(43, 132, 9, 1.0);
     } else {
       buttonBackColor = const Color.fromRGBO(23, 23, 23, 1.0);
     }
-    return Padding(
-      padding: const EdgeInsets.all(7.0),
-      child: CircleAvatar(
-        backgroundColor: buttonBackColor,
-        radius: MediaQuery.of(context).size.height * 0.1 * buttonHeight / 1.8,
-        child: TextButton(
-          onPressed: () => buttonPressed(buttonText),
-          child: Text(
-            buttonText,
-            style: TextStyle(
-              fontSize: fontSize,
-              fontWeight: FontWeight.normal,
-              color: buttonColor,
+
+    final radius = BorderRadius.circular(
+        MediaQuery.of(context).size.height * 0.1 / 1.8);
+
+    return Expanded(
+      flex: flex,
+      child: Padding(
+        padding: const EdgeInsets.all(7.0),
+        child: SizedBox(
+          height: radius.bottomLeft.x * 1.8,
+          child: Material(
+            color: buttonBackColor,
+            borderRadius: radius,
+            child: InkWell(
+              onTap: () => buttonPressed(buttonText),
+              borderRadius: radius,
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  buttonText,
+                  style: TextStyle(
+                    fontSize: fontSize,
+                    fontWeight: FontWeight.normal,
+                    color: buttonColor,
+                  ),
+                ),
+              ),
             ),
           ),
         ),
@@ -207,70 +206,49 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Container(
                 alignment: Alignment.bottomRight,
                 child: Text(
-                  "$result\n$equation",
-                  style: const TextStyle(fontSize: 30, color: Colors.white),
+                  result,
+                  style: const TextStyle(fontSize: 56, color: Colors.white),
                 ),
               ),
             ),
           ),
           const Padding(
-            padding: EdgeInsets.symmetric(vertical: 20),
-            child: Divider(
-                thickness: 1.8,
-                color: Color.fromRGBO(45, 45, 45, 1.0)),
+            padding: EdgeInsets.symmetric(vertical: 3),
+            child:
+                Divider(thickness: 1.8, color: Color.fromRGBO(45, 45, 45, 1.0)),
           ),
           Row(children: [
-            Padding(
-              padding: const EdgeInsets.all(7.0),
-              child: Container(
-                color: Colors.transparent,
-                child: Container(
-                    width: MediaQuery.of(context).size.height * 0.1 * 2.2 * 1.5 + 29,
-                    height: MediaQuery.of(context).size.height * 0.1,
-                    decoration: const BoxDecoration(
-                        color: Color.fromRGBO(23, 23, 23, 1.0),
-                        borderRadius:
-                        BorderRadius.all(Radius.circular(99))),
-                    child: Center(
-                      child: TextButton(
-                        onPressed: () => buttonPressed("C"),
-                        child: const Text(
-                          "C", style: TextStyle(
-                            fontSize: 40,
-                            color: Color.fromRGBO(245, 116, 117, 1)
-                            // color: Colors.white
-                          ),
-                        ),
-                      ),
-                    )),
-              ),
-            ),
-            buildButton(
-                "÷", 1, const Color.fromRGBO(116, 202, 62, 1.0), 60),
+            buildButton('C', const Color.fromRGBO(245, 116, 117, 1),
+                flex: 3),
+            buildButton("÷", const Color.fromRGBO(116, 202, 62, 1.0),
+                fontSize: 60),
           ]),
           Row(children: [
-            buildButton("7", 1, Colors.white),
-            buildButton("8", 1, Colors.white),
-            buildButton("9", 1, Colors.white),
-            buildButton("×", 1, const Color.fromRGBO(116, 202, 62, 1.0), 60),
+            buildButton("7", Colors.white),
+            buildButton("8", Colors.white),
+            buildButton("9", Colors.white),
+            buildButton("×", const Color.fromRGBO(116, 202, 62, 1.0),
+                fontSize: 60),
           ]),
           Row(children: [
-            buildButton("4", 1, Colors.white),
-            buildButton("5", 1, Colors.white),
-            buildButton("6", 1, Colors.white),
-            buildButton("−", 1, const Color.fromRGBO(116, 202, 62, 1.0), 60),
+            buildButton("4", Colors.white),
+            buildButton("5", Colors.white),
+            buildButton("6", Colors.white),
+            buildButton("−", const Color.fromRGBO(116, 202, 62, 1.0),
+                fontSize: 60),
           ]),
           Row(children: [
-            buildButton("1", 1, Colors.white),
-            buildButton("2", 1, Colors.white),
-            buildButton("3", 1, Colors.white),
-            buildButton("+", 1, const Color.fromRGBO(116, 202, 62, 1.0), 60),
+            buildButton("1", Colors.white),
+            buildButton("2", Colors.white),
+            buildButton("3", Colors.white),
+            buildButton("+", const Color.fromRGBO(116, 202, 62, 1.0),
+                fontSize: 60),
           ]),
           Row(children: [
-            buildButton("+/-", 1, Colors.white),
-            buildButton("0", 1, Colors.white),
-            buildButton(".", 1, Colors.white, 50),
-            buildButton("=", 1, Colors.white, 60),
+            buildButton("+/-", Colors.white),
+            buildButton("0", Colors.white),
+            buildButton(".", Colors.white, fontSize: 50),
+            buildButton("=", Colors.white, fontSize: 60),
           ]),
         ],
       ),
